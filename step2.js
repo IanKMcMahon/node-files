75390ebae2378ba1bcb2d62fe31b8fa0a8519718
Your Name <you@example.com>
2024-01-03 01:47:02 -0800

    step2

---
+++ step2.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const process = require('process');
+
+
+function cat(path){
+
+fs.readFile(path, 'utf8', function(err, data) {
+    if(err) {
+        console.error(`Error reading${path}: ${err}`);
+        process.exit(1)
+    }
+    console.log(data)
+});
+}
+cat(process.argv[2]);
+
+
+async function webCat(url) {
+try {
+  let res = await axios.get(url);
+  console.log(res.data)
+}catch (err) {
+    console.error(`Error fetching ${url}: ${err}`);
+    process.exit(1);
+}
+}
