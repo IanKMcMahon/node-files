54c6ce169984d30162a441007fe9a872de48d330
Your Name <you@example.com>
2024-01-03 02:02:49 -0800

    add function call

--- step2.js
+++ step2.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const process = require('process');
+const axios = require('axios');
 
 
 function cat(path){
@@ -16,11 +17,19 @@ cat(process.argv[2]);
 
 
 async function webCat(url) {
-try {
-  let res = await axios.get(url);
-  console.log(res.data)
-}catch (err) {
-    console.error(`Error fetching ${url}: ${err}`);
-    process.exit(1);
+    try {
+      let res = await axios.get(url);
+      console.log(res.data)
+    }catch (err) {
+        console.error(`Error fetching ${url}: ${err}`);
+        process.exit(1);
+    }
 }
+
+let path = process.argv[2];
+
+if (path.slice(0, 4) === 'http') {
+  webCat(path);
+} else {
+  cat(path);
 }
